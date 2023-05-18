43c9330bf89b208af1d3242837db2828ff353a0f
lauk20 <72945813+lauk20@users.noreply.github.com>
2023-05-17 21:55:46 -0400

    Add basic non-null check for API request

--- packages/weather_api/test/weather_api_test.dart
+++ packages/weather_api/test/weather_api_test.dart
@@ -1,7 +1,17 @@
 import 'package:flutter_test/flutter_test.dart';
-
 import 'package:weather_api/weather_api.dart';
 
-void main() {
+bool nonNullTypeCheck(dynamic obj) {
+  return obj != null;
+}
 
+void main() {
+  test('Non-Null Check', () async {
+    WeatherAPIClient client = WeatherAPIClient();
+    final LocationData loc = await client.getLocation("New York City");
+    final CityData city = loc.results[0];
+    final WeatherData weather = await client.getWeather(latitude: city.latitude, longitude: city.longitude);
+
+    expect(weather, nonNullTypeCheck);
+  });
 }
