3795c087b2a2777701d245a5039103ed20a06178
lauk20 <72945813+lauk20@users.noreply.github.com>
2023-05-18 21:56:04 -0400

    add daily_forecast model

---
+++ packages/weather_repository/lib/src/models/daily_forecast.dart
@@ -0,0 +1,29 @@
+import 'package:equatable/equatable.dart';
+import 'package:json_annotation/json_annotation.dart';
+import 'weather_code.dart';
+
+part 'daily_forecast.g.dart';
+
+@JsonSerializable(fieldRename: FieldRename.snake)
+class DailyForecast extends Equatable {
+  final String location;
+  final List<double> temperaturesMax;
+  final List<double> temperaturesMin;
+  final List<double> precipitationProbabilities;
+  final List<WeatherCode> weatherCodes;
+
+  const DailyForecast({
+    required this.location,
+    required this.temperaturesMax,
+    required this.temperaturesMin,
+    required this.precipitationProbabilities,
+    required this.weatherCodes,
+  });
+
+  factory DailyForecast.fromJson(Map<String, dynamic> json) => _$DailyForecastFromJson(json);
+
+  Map<String, dynamic> toJson() => _$DailyForecastToJson(this);
+
+  @override
+  List<Object> get props => [location, temperaturesMax, temperaturesMin, precipitationProbabilities, weatherCodes];
+}
---
+++ packages/weather_repository/lib/src/models/daily_forecast.g.dart
@@ -0,0 +1,47 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'daily_forecast.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+DailyForecast _$DailyForecastFromJson(Map<String, dynamic> json) =>
+    DailyForecast(
+      location: json['location'] as String,
+      temperaturesMax: (json['temperatures_max'] as List<dynamic>)
+          .map((e) => (e as num).toDouble())
+          .toList(),
+      temperaturesMin: (json['temperatures_min'] as List<dynamic>)
+          .map((e) => (e as num).toDouble())
+          .toList(),
+      precipitationProbabilities:
+          (json['precipitation_probabilities'] as List<dynamic>)
+              .map((e) => (e as num).toDouble())
+              .toList(),
+      weatherCodes: (json['weather_codes'] as List<dynamic>)
+          .map((e) => $enumDecode(_$WeatherCodeEnumMap, e))
+          .toList(),
+    );
+
+Map<String, dynamic> _$DailyForecastToJson(DailyForecast instance) =>
+    <String, dynamic>{
+      'location': instance.location,
+      'temperatures_max': instance.temperaturesMax,
+      'temperatures_min': instance.temperaturesMin,
+      'precipitation_probabilities': instance.precipitationProbabilities,
+      'weather_codes':
+          instance.weatherCodes.map((e) => _$WeatherCodeEnumMap[e]!).toList(),
+    };
+
+const _$WeatherCodeEnumMap = {
+  WeatherCode.clearSky: 'clearSky',
+  WeatherCode.mainlyClear: 'mainlyClear',
+  WeatherCode.partlyCloudy: 'partlyCloudy',
+  WeatherCode.overcast: 'overcast',
+  WeatherCode.fog: 'fog',
+  WeatherCode.drizzle: 'drizzle',
+  WeatherCode.rain: 'rain',
+  WeatherCode.snow: 'snow',
+  WeatherCode.thunderstorm: 'thunderstorm',
+};
