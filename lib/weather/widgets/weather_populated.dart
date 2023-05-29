d72e9bfd92591fa593c22fcb822b28849daaad94
lauk20 <72945813+lauk20@users.noreply.github.com>
2023-05-29 14:13:10 -0400

    add pull-down refresh

--- lib/weather/widgets/weather_populated.dart
+++ lib/weather/widgets/weather_populated.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:windchimes/weather/weather.dart';
 import 'package:windchimes/weather/widgets/weather_icons.dart';
 import 'dart:developer' as developer;
@@ -26,136 +27,144 @@ class WeatherPopulated extends StatelessWidget {
 
     developer.log(hourlyWeather.times.toString());
 
-    return SingleChildScrollView(
-      child: Column(
-        mainAxisSize: MainAxisSize.max,
-        mainAxisAlignment: MainAxisAlignment.start,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          // Current Weather
-          Padding(
-            padding: const EdgeInsets.only(left: 24, right: 24, top: 24, bottom: 32),
-            child: IntrinsicHeight(
-              child: Row(
-                children: [
-                  ConstrainedBox(
-                    constraints: BoxConstraints(
-                      minWidth: MediaQuery.of(context).size.width * 0.5
-                    ),
-                    //flex: 1,
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          location.name,
-                          style: theme.textTheme.titleSmall,
-                          overflow: TextOverflow.ellipsis,
-                          softWrap: false,
-                        ),
-                        Text(
-                          "${currentWeather.temperature.toInt().toString()}°",
-                          style: theme.textTheme.displayLarge,
-                        ),
-                        Text(
-                          currentWeather.weathercode,
-                          style: theme.textTheme.titleLarge,
-                          overflow: TextOverflow.clip,
-                        ),
-                      ],
-                    ),
-                  ),
-                  Expanded(
-                    child: FittedBox(
-                      child: currentWeather.weathercode.getIcon()
-                    )
-                  ),
-                ]
-              )
-            )
-          ),
-
-          // Hourly Forecast Card
-          Padding(
-            padding: const EdgeInsets.only(left: 16, right: 16),
-            child: Card(
-              elevation: 0,
-              color: theme.colorScheme.onPrimary,
-              //color: const Color.fromRGBO(48, 48, 48, 1),
-              child: FractionallySizedBox(
-                widthFactor: 1,
-                child: Column(
+    return RefreshIndicator(
+      onRefresh: () async {
+        WeatherCubit wc = BlocProvider.of<WeatherCubit>(context);
+        await wc.getWeather(wc.state.selectedCity);
+      },
+      child: SingleChildScrollView(
+        physics: const AlwaysScrollableScrollPhysics(),
+        clipBehavior: Clip.none,
+        child: Column(
+          mainAxisSize: MainAxisSize.max,
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            // Current Weather
+            Padding(
+              padding: const EdgeInsets.only(left: 24, right: 24, top: 24, bottom: 32),
+              child: IntrinsicHeight(
+                child: Row(
                   children: [
-                    SizedBox(
-                      height: 160,
-                      child: Padding(
-                        padding: const EdgeInsets.only(left: 16, right: 16),
-                        child: FadingListView(
-                          child: ListView(
-                            scrollDirection: Axis.horizontal,
-                            children: hourlyWeather.times.asMap().entries.map((e) {
+                    ConstrainedBox(
+                      constraints: BoxConstraints(
+                        minWidth: MediaQuery.of(context).size.width * 0.5
+                      ),
+                      //flex: 1,
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            location.name,
+                            style: theme.textTheme.titleSmall,
+                            overflow: TextOverflow.ellipsis,
+                            softWrap: false,
+                          ),
+                          Text(
+                            "${currentWeather.temperature.toInt().toString()}°",
+                            style: theme.textTheme.displayLarge,
+                          ),
+                          Text(
+                            currentWeather.weathercode,
+                            style: theme.textTheme.titleLarge,
+                            overflow: TextOverflow.clip,
+                          ),
+                        ],
+                      ),
+                    ),
+                    Expanded(
+                      child: FittedBox(
+                        child: currentWeather.weathercode.getIcon()
+                      )
+                    ),
+                  ]
+                )
+              )
+            ),
+
+            // Hourly Forecast Card
+            Padding(
+              padding: const EdgeInsets.only(left: 16, right: 16),
+              child: Card(
+                elevation: 0,
+                color: theme.colorScheme.onPrimary,
+                //color: const Color.fromRGBO(48, 48, 48, 1),
+                child: FractionallySizedBox(
+                  widthFactor: 1,
+                  child: Column(
+                    children: [
+                      SizedBox(
+                        height: 160,
+                        child: Padding(
+                          padding: const EdgeInsets.only(left: 16, right: 16),
+                          child: FadingListView(
+                            child: ListView(
+                              scrollDirection: Axis.horizontal,
+                              children: hourlyWeather.times.asMap().entries.map((e) {
+                                int index = e.key;
+                                String time = e.value;
+                                
+                                return HourlyWeatherCard(
+                                  temperature: hourlyWeather.temperatures[index].toInt(), 
+                                  precipitation: hourlyWeather.precipitationProbabilities[index].toInt(),
+                                  weathercode: hourlyWeather.weatherCodes[index],
+                                  time: time,
+                                );
+                              }).toList(),
+                            )
+                          )
+                        )
+                      )
+                    ],
+                  )
+                )
+              )
+            ),
+
+            // Daily Forecast Card
+            Padding(
+              padding: const EdgeInsets.only(top: 8, left: 16, right: 16, bottom: 16),
+              child: Card(
+                elevation: 0,
+                color: theme.colorScheme.onPrimary,
+                //color: const Color.fromRGBO(48, 48, 48, 1),
+                child: FractionallySizedBox(
+                  widthFactor: 1,
+                  child: Column(
+                    children: [
+                      SizedBox(
+                        height: 320,
+                        child: Padding(
+                          padding: const EdgeInsets.only(left: 16, right: 16),
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.spaceAround,
+                            children: dailyWeather.times.asMap().entries.map((e) {
                               int index = e.key;
                               String time = e.value;
+
+                              developer.log(dailyWeather.toString());
+                              developer.log(hourlyWeather.precipitationProbabilities[index].toString());
+                              developer.log(hourlyWeather.precipitationProbabilities[index].toInt().toString());
                               
-                              return HourlyWeatherCard(
-                                temperature: hourlyWeather.temperatures[index].toInt(), 
-                                precipitation: hourlyWeather.precipitationProbabilities[index].toInt(),
-                                weathercode: hourlyWeather.weatherCodes[index],
-                                time: time,
+                              return DailyWeatherCard(
+                                minTemp: dailyWeather.temperaturesMin[index].toInt().toString(), 
+                                maxTemp: dailyWeather.temperaturesMax[index].toInt().toString(),
+                                precipitation: dailyWeather.precipitationProbabilities[index].toInt().toString(),
+                                weathercode: dailyWeather.weatherCodes[index],
+                                day: index == 0 ? "Today" : time,
                               );
                             }).toList(),
                           )
                         )
                       )
-                    )
-                  ],
+                    ],
+                  )
                 )
               )
             )
-          ),
-
-          // Daily Forecast Card
-          Padding(
-            padding: const EdgeInsets.only(top: 8, left: 16, right: 16),
-            child: Card(
-              elevation: 0,
-              color: theme.colorScheme.onPrimary,
-              //color: const Color.fromRGBO(48, 48, 48, 1),
-              child: FractionallySizedBox(
-                widthFactor: 1,
-                child: Column(
-                  children: [
-                    SizedBox(
-                      height: 320,
-                      child: Padding(
-                        padding: const EdgeInsets.only(left: 16, right: 16),
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.spaceAround,
-                          children: dailyWeather.times.asMap().entries.map((e) {
-                            int index = e.key;
-                            String time = e.value;
-
-                            developer.log(dailyWeather.toString());
-                            developer.log(hourlyWeather.precipitationProbabilities[index].toString());
-                            developer.log(hourlyWeather.precipitationProbabilities[index].toInt().toString());
-                            
-                            return DailyWeatherCard(
-                              minTemp: dailyWeather.temperaturesMin[index].toInt().toString(), 
-                              maxTemp: dailyWeather.temperaturesMax[index].toInt().toString(),
-                              precipitation: dailyWeather.precipitationProbabilities[index].toInt().toString(),
-                              weathercode: dailyWeather.weatherCodes[index],
-                              day: index == 0 ? "Today" : time,
-                            );
-                          }).toList(),
-                        )
-                      )
-                    )
-                  ],
-                )
-              )
-            )
-          )
-        ],
+          ],
+        )
       )
     );
   }
