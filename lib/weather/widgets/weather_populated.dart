a719f38c8912579d10b819b50b6f56a87ab28a4a
lauk20 <72945813+lauk20@users.noreply.github.com>
2023-05-29 12:57:53 -0400

    use constrainedbox to establish min width for current weather info

--- lib/weather/widgets/weather_populated.dart
+++ lib/weather/widgets/weather_populated.dart
@@ -38,8 +38,11 @@ class WeatherPopulated extends StatelessWidget {
             child: IntrinsicHeight(
               child: Row(
                 children: [
-                  FittedBox(
-                    //flex: 2,
+                  ConstrainedBox(
+                    constraints: BoxConstraints(
+                      minWidth: MediaQuery.of(context).size.width * 0.5
+                    ),
+                    //flex: 1,
                     child: Column(
                       mainAxisAlignment: MainAxisAlignment.start,
                       crossAxisAlignment: CrossAxisAlignment.start,
@@ -197,5 +200,32 @@ extension on Color {
 extension on Weather {
   String formattedTemperature(TemperatureUnits units) {
     return '''${currentWeather.temperature.toStringAsPrecision(2)}°${units == TemperatureUnits.celsius ? 'C' : 'F'}''';
+  }
+}
+
+extension on WeatherCode {
+  Icon getIcon() {
+    switch(this) {
+      case (WeatherCode.clearSky):
+        return WeatherIcons.sunnyIcon;
+      case (WeatherCode.mainlyClear):
+        return WeatherIcons.sunnyIcon;
+      case (WeatherCode.partlyCloudy):
+        return WeatherIcons.partlyCloudyIcon;
+      case (WeatherCode.overcast):
+        return WeatherIcons.cloudyIcon;
+      case (WeatherCode.fog):
+        return WeatherIcons.foggyIcon;
+      case (WeatherCode.drizzle):
+        return WeatherIcons.rainIcon;
+      case (WeatherCode.rain):
+        return WeatherIcons.rainIcon;
+      case (WeatherCode.snow):
+        return WeatherIcons.snowIcon;
+      case (WeatherCode.thunderstorm):
+        return WeatherIcons.thunderstormIcon;
+      default:
+        return WeatherIcons.sunnyIcon;
+    }
   }
 }
