cf2cd9ba573708771ecb7158e7a65b1c25b27cb2
lauk20 <72945813+lauk20@users.noreply.github.com>
2023-06-11 15:00:04 -0400

    restyle weather error page

--- lib/weather/widgets/weather_failure.dart
+++ lib/weather/widgets/weather_failure.dart
@@ -6,11 +6,23 @@ class WeatherFailure extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
-    return Column(
-      mainAxisSize: MainAxisSize.min,
-      children: [
-        Text('Something went wrong!', style: theme.textTheme.headlineMedium)
-      ],
+    return Center(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Text(
+            'Something went wrong!',
+            style: theme.textTheme.titleLarge
+          ),
+          Padding(
+            padding: const EdgeInsets.all(16),
+            child: Text(
+              'Try reloading the app or checking your connection!',
+              style: theme.textTheme.titleSmall
+            ),
+          ),
+        ],
+      )
     );
   }
 }
